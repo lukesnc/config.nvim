de4c3289e8ac2856f7253919eed1fe9670bcffdd
lukesnc <lukeasimone@gmail.com>
2024-05-20 18:23:12 -0400

    disable linechars

--- init.lua
+++ init.lua
@@ -13,8 +13,8 @@ vim.opt.smartcase = true
 vim.opt.signcolumn = "yes" -- Keep signcolumn on by default
 vim.opt.updatetime = 250 -- Decrease update time
 vim.opt.timeoutlen = 300 -- Decrease mapped sequence wait time, Displays which-key popup sooner
-vim.opt.list = true -- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+-- vim.opt.list = true -- Sets how neovim will display certain whitespace characters in the editor.
+-- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
 vim.opt.cursorline = true -- Show which line your cursor is on
 vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
