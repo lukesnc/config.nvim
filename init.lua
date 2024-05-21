b0bf319be07e6413c02ba20aa2951ab2e8835500
lukesnc <lukeasimone@gmail.com>
2024-05-20 23:31:13 -0400

    remove call to Comment (removed plugin)

--- init.lua
+++ init.lua
@@ -52,10 +52,7 @@ require("lazy").setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme("rose-pine-moon")
-
-      -- You can configure highhlights by doing something like:
-      vim.cmd.hi("Comment gui=none")
+      vim.cmd.colorscheme("rose-pine")
     end,
   },
 
