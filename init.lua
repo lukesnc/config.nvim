2ed461d0ca778bce778f699405f7e22bb78538bf
lukesnc <lukeasimone@gmail.com>
2024-05-26 20:17:58 -0400

    add python formatting

--- init.lua
+++ init.lua
@@ -162,6 +162,7 @@ require("lazy").setup({
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
+        python = { "isort", "black" },
       },
       format_on_save = {
         -- These options will be passed to conform.format()
