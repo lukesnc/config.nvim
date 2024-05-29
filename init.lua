238d04fca61c6f9de4c4ff36e81ac479f3ca3e50
lukesnc <lukeasimone@gmail.com>
2024-05-29 16:52:43 -0400

    removed unused opts and comments

--- init.lua
+++ init.lua
@@ -1,21 +1,18 @@
--- Large thanks to kickstart.nvim
-
+-- Opts
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.opt.number = true -- Line nums on
-vim.opt.relativenumber = true -- Relative line nums
-vim.opt.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.showmode = false -- Don't show the mode, since it's already in the status line
-vim.opt.breakindent = true -- Enable break indent
-vim.opt.undofile = true -- Save undo history
-vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = "yes" -- Keep signcolumn on by default
-vim.opt.updatetime = 250 -- Decrease update time
-vim.opt.timeoutlen = 300 -- Decrease mapped sequence wait time, Displays which-key popup sooner
-vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
-vim.opt.cursorline = true -- Show which line your cursor is on
-vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.inccommand = "split"
+vim.opt.scrolloff = 8
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -23,7 +20,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -79,7 +76,7 @@ require("lazy").setup({
     },
   },
 
-  { -- Telescope fuzzy finder
+  { -- Telescope
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
