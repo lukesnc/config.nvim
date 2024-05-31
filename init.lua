b6fbb8d8f95d933753ecf9029c48f948df8841d7
lukesnc <lukeasimone@gmail.com>
2024-05-30 20:27:57 -0400

    fix splits

--- init.lua
+++ init.lua
@@ -13,6 +13,8 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.inccommand = "split"
 vim.opt.scrolloff = 10
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
