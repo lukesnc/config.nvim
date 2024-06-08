818507f3a48bd5c8239a52c23a470c47ea284a6b
lukesnc <lukeasimone@gmail.com>
2024-06-08 02:49:06 -0400

    revert to prev cmp keybinds

--- init.lua
+++ init.lua
@@ -186,11 +186,11 @@ require("lazy").setup({
           end,
         },
         mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = {
           { name = "nvim_lsp" },
