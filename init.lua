0443a45f4712024ded9fd7d3a69f7a1069f05cd0
lukesnc <lukeasimone@gmail.com>
2024-07-27 14:53:16 -0400

    chore: update install recipes

--- init.lua
+++ init.lua
@@ -31,14 +31,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Install Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -54,7 +57,7 @@ require("lazy").setup({
       },
     },
     init = function()
-      vim.cmd.colorscheme("rose-pine")
+      vim.cmd("colorscheme rose-pine")
     end,
   },
 
