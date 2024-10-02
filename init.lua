5ab3788d5ad68aea8be5bdf06f6330b88900e8ad
lukesnc <lukeasimone@gmail.com>
2024-10-02 01:18:41 -0400

    remove spec table since not using any opts

--- init.lua
+++ init.lua
@@ -47,163 +47,161 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    { -- Colorscheme
-      "rose-pine/nvim",
-      name = "rose-pine",
-      priority = 1000,
-      opts = {
-        styles = {
-          italic = false,
-        },
+  { -- Colorscheme
+    "rose-pine/nvim",
+    name = "rose-pine",
+    priority = 1000,
+    opts = {
+      styles = {
+        italic = false,
       },
-      init = function()
-        vim.cmd("colorscheme rose-pine")
-      end,
     },
+    init = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
 
-    -- Detect tabstop and shiftwidth automatically
-    "tpope/vim-sleuth",
+  -- Detect tabstop and shiftwidth automatically
+  "tpope/vim-sleuth",
 
-    -- Gitsigns
-    { "lewis6991/gitsigns.nvim", opts = {} },
+  -- Gitsigns
+  { "lewis6991/gitsigns.nvim", opts = {} },
 
-    { -- LSP Config + Mason
-      "neovim/nvim-lspconfig",
-      dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-      },
-      config = function()
-        vim.api.nvim_create_autocmd("LspAttach", {
-          callback = function(event)
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf })
+  { -- LSP Config + Mason
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf })
+        end,
+      })
+
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+        },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({
+              capabilities = capabilities,
+            })
           end,
-        })
-
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            "lua_ls",
-          },
-          handlers = {
-            function(server_name)
-              require("lspconfig")[server_name].setup({
-                capabilities = capabilities,
-              })
-            end,
-          },
-        })
-      end,
-    },
-
-    { -- LuaLS setup
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "luvit-meta/library", words = { "vim%.uv" } },
         },
+      })
+    end,
+  },
+
+  { -- LuaLS setup
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
       },
     },
+  },
 
-    -- Fidget notif + LSP messages
-    { "j-hui/fidget.nvim", opts = {} },
+  -- Fidget notif + LSP messages
+  { "j-hui/fidget.nvim", opts = {} },
 
-    { -- Autoformat
-      "stevearc/conform.nvim",
-      opts = {
-        formatters_by_ft = {
-          lua = { "stylua" },
-          markdown = { "markdownlint" },
-        },
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_format = "fallback",
-        },
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        markdown = { "markdownlint" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
       },
     },
+  },
 
-    { -- Autocompletion
-      "hrsh7th/nvim-cmp",
-      event = "InsertEnter",
-      dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-path",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-      },
-      config = function()
-        local cmp = require("cmp")
-
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              require("luasnip").lsp_expand(args.body)
-            end,
-          },
-          mapping = cmp.mapping.preset.insert({
-            ["<C-n>"] = cmp.mapping.select_next_item(),
-            ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          }),
-          sources = {
-            { name = "nvim_lsp" },
-            { name = "luasnip" },
-            { name = "path" },
-          },
-        })
-      end,
+  { -- Autocompletion
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
     },
+    config = function()
+      local cmp = require("cmp")
 
-    { -- Treesitter
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        local configs = require("nvim-treesitter.configs")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
 
-        configs.setup({
-          ensure_installed = { "bash", "c", "python", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-          sync_install = false,
-          auto_install = true,
-          highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
+  { -- Treesitter
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "bash", "c", "python", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
+  },
+
+  { -- Telescope
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            ".git/",
+            "node_modules",
           },
-        })
-      end,
-    },
-
-    { -- Telescope
-      "nvim-telescope/telescope.nvim",
-      branch = "0.1.x",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-        require("telescope").setup({
-          defaults = {
-            file_ignore_patterns = {
-              ".git/",
-              "node_modules",
-            },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
           },
-          pickers = {
-            find_files = {
-              hidden = true,
-            },
-          },
-        })
+        },
+      })
 
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-      end,
-    },
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    end,
   },
 })
