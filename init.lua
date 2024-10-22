2c480d92c9c9bfc060f8b751959a1567bfbfcf38
lukesnc <lukeasimone@gmail.com>
2024-10-22 18:20:57 -0400

    add cmp-buffer to autocompletion

--- init.lua
+++ init.lua
@@ -132,6 +132,7 @@ local plugins = {
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
     },
@@ -155,6 +156,7 @@ local plugins = {
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "path" },
+          { name = "buffer" },
         },
       })
     end,
