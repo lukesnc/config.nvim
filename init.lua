f69049642f4be34e4d1ce127037164c2d70e6353
lukesnc <35816797+lukesnc@users.noreply.github.com>
2025-06-29 18:04:54 -0400

    remove settings i wasnt using

--- init.lua
+++ init.lua
@@ -1,25 +1,24 @@
 -- Opts
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.inccommand = "split"
-vim.opt.scrolloff = 10
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.clipboard = "unnamedplus"
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.scrolloff = 10
+
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+vim.opt.clipboard = "unnamedplus"
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
